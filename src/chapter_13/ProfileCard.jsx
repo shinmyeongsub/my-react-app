e2a298b9b1c63907ce8f893411c07444ab5246e0
MyeongSeop Shin <83411963+shinmyeongsub@users.noreply.github.com>
2024-05-07 00:38:33 +0900

    Feature : Chapter_13 -> Composition vs Inheritance Chapter_14 -> Context

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return (
+        <Card title="Aiden Shin" backgroundColor="yellow">
+            <p>안녕하세요, 에이든입니다.</p>
+            <p>저는 리액트를 배우는 중입니다.</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
--- src/index.js
+++ src/index.js
@@ -10,14 +10,14 @@ import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
 import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/ProfileCard';
+import DarkOrLight from './chapter_14/DarkOrLight';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <AttendanceBook/> */}
-    {/* <SignUp></SignUp> */}
-    <Calculator/>
+    <DarkOrLight/>
   </React.StrictMode>,
   document.getElementById('root')
 );
